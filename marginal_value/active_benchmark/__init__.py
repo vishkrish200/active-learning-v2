2fd461a2f3fdf20bc4c2d0e77e4374153445bd63
vishkrish200 <vishkrish200@gmail.com>
2026-05-06 01:20:42 +0530

    Add offline benchmark discrimination audit

--- marginal_value/active_benchmark/__init__.py
+++ marginal_value/active_benchmark/__init__.py
@@ -1,4 +1,8 @@
 from marginal_value.active_benchmark.benchmark_decision import build_benchmark_decision_report, write_benchmark_decision_reports
+from marginal_value.active_benchmark.benchmark_discrimination import (
+    build_benchmark_discrimination_report,
+    write_benchmark_discrimination_reports,
+)
 from marginal_value.active_benchmark.reports import write_benchmark_reports
 from marginal_value.active_benchmark.runner import run_offline_active_benchmark
 from marginal_value.active_benchmark.schema import BenchmarkClip, BenchmarkResult, EpisodeSpec, OfflineBenchmarkConfig, RoundResult
@@ -11,9 +15,11 @@ __all__ = [
     "OfflineBenchmarkConfig",
     "RoundResult",
     "build_benchmark_decision_report",
+    "build_benchmark_discrimination_report",
     "build_difficulty_targeted_episodes",
     "build_source_blocked_episodes",
     "run_offline_active_benchmark",
     "write_benchmark_decision_reports",
+    "write_benchmark_discrimination_reports",
     "write_benchmark_reports",
 ]
